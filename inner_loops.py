94e0897375625363114dcf66edf3e6964e495f37
iharshcheema <harshcheemaweb@gmail.com>
2024-08-19 23:55:05 +0530

    two sum

---
+++ inner_loops.py
@@ -0,0 +1,33 @@
+# for y in range(3):
+#     for x in range(4):
+#         print(x)   
+
+
+# for y in range(3):
+#     for x in range(4):
+#         print(x)  
+#     print()   
+
+
+# for y in range(3):
+#     for x in range(0,4):
+#         print(x , end="")   
+
+
+# for y in range(3):
+#     for x in range(0,4):
+#         print(x , end="") 
+#     print()       
+
+
+for y in range(3):
+    for x in range(0,4):
+        print('*', end="") 
+    print()       
+# the inner loops is resposible for printing the stars in a single line 
+# after we exit the loop we print a line line thats is why the next inner loop is executed in the second line
+# and the outer loops makes the above 2 step repeat for 3 times    
+
+
+      
+    
